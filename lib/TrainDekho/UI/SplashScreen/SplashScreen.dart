7f6598143180b5df0922c190e3099f8c53e3fc04
Kishan Kumar <infokkishan@gmail.com>
2024-06-09 16:35:42 +0530

    first commit

---
+++ lib/TrainDekho/UI/SplashScreen/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:train_dekho/TrainDekho/UI/Home/Home.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 10), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const HomeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset('assets/splashScreen.json'),
+      ),
+    );
+  }
+}
+
+
